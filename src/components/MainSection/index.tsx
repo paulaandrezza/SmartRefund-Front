866bbaa9b0541684d10c614cc1e4fe7a4be0be20
paulaandrezza <paulaandrezza25@gmail.com>
2024-03-16 18:09:09 -0300

    feat: add search input

--- src/components/MainSection/index.tsx
+++ src/components/MainSection/index.tsx
@@ -1,5 +1,5 @@
-import { Add } from "@mui/icons-material";
-import { Box, Button } from "@mui/material";
+import { Add, Search } from "@mui/icons-material";
+import { Box, Button, InputAdornment, TextField } from "@mui/material";
 import React from "react";
 import { AddReciptModal } from "../AddReciptModal";
 import { RefundCard } from "../RefundCard";
@@ -15,6 +15,11 @@ const recipData = {
 
 export const MainSection = () => {
   const [openModal, setOpenModal] = React.useState(false);
+  const [showClearIcon, setShowClearIcon] = React.useState("none");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setShowClearIcon(event.target.value === "" ? "none" : "flex");
+  };
 
   return (
     <>
@@ -23,7 +28,27 @@ export const MainSection = () => {
         className="h-full p-4 flex flex-col gap-4"
         bgcolor="primary"
       >
-        <div className="flex justify-end">
+        <div className="flex justify-end gap-4">
+          <TextField
+            className="w-72"
+            variant="outlined"
+            label="Pesquisar pelo hash"
+            placeholder="Insira a hash da nota fiscal"
+            type="search"
+            onChange={handleChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <Button variant="contained" color="secondary">
+            Pesquisar
+          </Button>
+
           <Button variant="contained" onClick={() => setOpenModal(true)}>
             <Add />
             Adicionar nota fiscal
