e3d45ab521a86b04e7633a31d3ff85e7046dd367
paulaandrezza <paulaandrezza25@gmail.com>
2024-03-17 22:17:46 -0300

    feat: add headers to api

--- src/services/api/index.tsx
+++ src/services/api/index.tsx
@@ -3,6 +3,10 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use(
